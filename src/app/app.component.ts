f48d802ca898aa67819aad7afb4adfe3797eb37c
Aditya Bhasin <conlini@gmail.com>
2017-10-21 20:02:42 +0530

    base upto mid routing

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector : 'my-app',
+	template : `
+	<nav>
+	<a routerLink="/dashboard">Dashboard</a>
+	<a routerLink="/heroes">Heroes</a>
+	</nav>
+	<router-outlet></router-outlet>
+	`
+})
+export class AppComponent {}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+{id: 11, name: "batman"},
+{id : 12 , name : "superman"},
+{id : 13, name : "iron man"},
+{id : 15, name : "captain america"},
+{id : 16, name : "hulk"},
+{id : 17, name : "robin"},
+];
